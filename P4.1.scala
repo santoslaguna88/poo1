2535672240b5c346ac1fce03d674151e947e473a
Misael <misael.albores.l711@hotmail.com>
2015-09-15 10:56:28 -0500

    Practica 4 [Los 2 Ejercicios]

---
+++ P4.1.scala
@@ -0,0 +1,21 @@
+﻿var total=0;
+var esApto=true;
+for(i<-1 to 10){
+   println("cual es el tiempo que te tardas en el dia " + i + ":   ");
+   val tiempo=readInt();
+   println(tiempo);
+   if(tiempo>16){
+       esApto=false;
+   }
+   total=total + tiempo;
+}
+println("el total de los 10 dias es : " + total);
+val promedio=total/10;
+println("tu promedio es: " + promedio);
+
+if(promedio<=15 && esApto==true){
+   println("si eres apto");
+}
+else{
+   println("elige otra especialidad");
+}
---
+++ P4.2.scala
@@ -0,0 +1,74 @@
+﻿var total1=0
+var total2=0
+var total3=0
+println("Que animal deseas estudiar(elefante, jirafa o chimpance)")
+val animal=readLine()
+println(animal)
+if(animal=="elefante"){
+  for(i<-1 to 20){
+    println("Cual es su edad del animal " + i + "? ")
+    val edad=readInt()
+    println(edad)
+    if(edad>=0 && edad<=1){
+      total1=total1+1
+    }
+    if(edad>1 && edad<3){
+      total2=total2+1
+    }
+    if(edad>=3){
+      total3=total3+1
+    }
+ }
+  val porcentaje1=(total1*100)/20
+  println("El porcentaje de animales en la categoria de 0 a 1 año: " + porcentaje1 + "%")
+  val porcentaje2=(total2*100)/20
+  println("El porcentaje de animales en la categoria mas de 1 y menor a 3 es: " + porcentaje2 + "%")
+  val porcentaje3=(total3*100)/20
+  println("El porcentaje de animales en la categoria de 3 o mas: " + porcentaje3 + "%")
+}
+
+if(animal=="jirafa"){
+  for(i<-1 to 15){
+    println("Cual es su edad del animal " + i + "? ")
+    val edad=readInt()
+    println(edad)
+    if(edad>=0 && edad<=1){
+      total1=total1+1
+    }
+    if(edad>1 && edad<3){
+      total2=total2+1
+    }
+    if(edad>=3){
+      total3=total3+1
+    }
+  }
+  val porcentaje1=(total1*100)/15
+  println("El porcentaje de animales en la categoria de 0 a 1 año: " + porcentaje1 + "%")
+  val porcentaje2=(total2*100)/15
+  println("El porcentaje de animales en la categoria mas de 1 y menor a 3 es: " + porcentaje2 + "%")
+  val porcentaje3=(total3*100)/15
+  println("El porcentaje de animales en la categoria de 3 o mas: " + porcentaje3 + "%")
+}
+
+if(animal=="chimpance"){
+  for(i<-1 to 40){
+    println("Cual es su edad del animal " + i + "? ")
+    val edad=readInt()
+    println(edad)
+    if(edad>=0 && edad<=1){
+      total1=total1+1
+    }
+    if(edad>1 && edad<3){
+      total2=total2+1
+    }
+    if(edad>=3){
+      total3=total3+1
+    }
+  }
+  val porcentaje1=(total1*100)/40
+  println("El porcentaje de animales en la categoria de 0 a 1 año: " + porcentaje1 + "%")
+  val porcentaje2=(total2*100)/40
+  println("El porcentaje de animales en la categoria mas de 1 y menor a 3 es: " + porcentaje2 + "%")
+  val porcentaje3=(total3*100)/40
+  println("El porcentaje de animales en la categoria de 3 o mas: " + porcentaje3 + "%")
+}
