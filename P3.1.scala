56e72e7553064b5dd03ecb737775b90ebad6da02
Misael <misael.albores.l711@hotmail.com>
2015-09-15 10:55:37 -0500

    Practica 3 [Los 2 Ejercicios]

---
+++ P3.1.scala
@@ -0,0 +1,48 @@
+﻿println("Descuentos")
+println("¿Cùal es el precio de su articulo?")
+val precio=readInt()
+println("Escoje una bolita para saber tu descuento")
+println("-Blanca")
+println("-Roja")
+println("-Azul")
+println("-Verde")
+println("-Amarilla")
+val bola=readLine()
+bola match{
+case "blanca" =>{
+ println("Lo siento. No se efecutuara descuento alguno.")
+ println("Su total a pagar sera de: ")
+ print(precio)
+ println("")
+}
+case "roja" =>{
+ println("Felicidades se llevara su penda con un descuento de 100%.")
+ println("Su total a pagar sera de: ")
+ val ptotal=precio-(precio*1)
+ print(ptotal)
+ println("")
+}
+case "azul" =>{
+ println("Sorprendente, usted tiene un 50/50, se le efectuara un descuento de 50%.")
+ println("Su total a pagar sera de: ")
+ val ptotal=precio-(precio*.5)
+ print(ptotal)
+ println("")
+}
+case "amarilla" =>{
+ println("Al menos no es un 10%, obtuvo un descuento de 25%.")
+ println("Su total a pagar sera de: ")
+ val ptotal=precio-(precio*.25)
+ print(ptotal)
+ println("")
+}
+case "verde" =>{
+ println("Pudo pagar todo. descuento de 10%.")
+ println("Su total a pagar sera de: ")
+ val ptotal=precio-(precio*.1)
+ print(ptotal)
+ println("")
+}
+case default => println("Bola inexistente. Se perdio la oportunidad de un descuento.")
+}
+println("Gracias por su compra.")
---
+++ P3.2.scala
@@ -0,0 +1,30 @@
+﻿println("¿Asì que se quiere jubilar? Dime tu nombre: ")
+val name= readLine()
+println("Hola " + name + " ahora necesitamos saber su edad: ")
+val edad=readFloat()
+if (edad<60){
+ println("Necesitamos saber su antiguedad en la empresa para contiunuar el proceso: ")
+ val anti=readFloat()
+ if(anti>=25){
+    println(name + " sus datos proporcionados son los siguientes: ")
+    println("Edad: " + edad + " Antigüedad: " + anti)
+    println("A usted le corresponde la jubilacion por: Antigüedad Joven. ")
+  } else {
+    println("No cumple con la edad o antiüedad requerida para la jubilacion.")
+    val ftiempo=25-anti
+    val fedad=60-edad
+    println("Requeriria " + fedad + " años o " + ftiempo + " de antigüedad. ")
+  }
+} else {
+ println("Ahora ingrese su antigüedad: ")
+ val anti=readFloat()
+ if(anti>=25){
+  println(name + " sus datos proporcionados son los siguientes: ")
+  println("Edad: " + edad + " Antigüedad: " + anti)
+  println("A usted le corresponde la jubilacion por: Antigüedad Adulta. ")
+ } else {
+  println(name + " sus datos proporcionados son los siguientes: ")
+  println("Edad: " + edad + " Antigüedad: " + anti)
+  println("A usted le corresponde la jubilacion por: Edad. ")
+ }
+}
