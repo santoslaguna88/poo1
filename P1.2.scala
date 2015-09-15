88857f35aeb941502cc346b123b7971f563cce64
Misael <misael.albores.l711@hotmail.com>
2015-09-15 10:19:47 -0500

    Practica 1 [Los 3 Ejercicios]

---
+++ P1.2.scala
@@ -0,0 +1,9 @@
+println ("Dame el presupues total")
+val PresupuestoTotalAnual=readFloat()
+
+val DineroGinecologia=(PresupuestoTotalAnual*.40)
+val DineroTraumatologia=(PresupuestoTotalAnual*.30)
+val DineroPediatria=(PresupuestoTotalAnual*.30)
+println  ("el presupuesto de ginecologia es " + DineroGinecologia)
+println  ("el presupuesto de traumatologia es " + DineroTraumatologia)
+println  ("el presupuesto de pediatria es " + DineroPediatria) 
